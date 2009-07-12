e036d9a4edf81099bdafb80c8fac660932857124
Scott Steadman <ss@stdmn.com>
2009-07-11 18:40:01 -0700

    Added SE scope to Unicode module.

--- test/se/unicode_test.rb
+++ test/se/unicode_test.rb
@@ -4,12 +4,12 @@ require "#{File.dirname(__FILE__)}/../../lib/se/unicode"
 class UnicodeTest < Test::Unit::TestCase
 
   def test_unicode
-    assert_equal "a", "#{Unicode::U0061}"
+    assert_equal "a", "#{SE::Unicode::U0061}"
   end
 
   def test_excepton_raised_on_invalid_unicode_value
     assert_raise NameError do
-      "#{Unicode::U65}"
+      "#{SE::Unicode::U65}"
     end
   end
 
