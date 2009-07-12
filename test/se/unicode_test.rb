e8812ded2bd2b5383204cd0557d3bc4adcd58e16
Scott Steadman <ss@stdmn.com>
2009-07-11 18:29:37 -0700

    Added Unicode module.

---
+++ test/se/unicode_test.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+require "#{File.dirname(__FILE__)}/../../lib/se/unicode"
+
+class UnicodeTest < Test::Unit::TestCase
+
+  def test_unicode
+    assert_equal "a", "#{Unicode::U0061}"
+  end
+
+  def test_excepton_raised_on_invalid_unicode_value
+    assert_raise NameError do
+      "#{Unicode::U65}"
+    end
+  end
+
+end
