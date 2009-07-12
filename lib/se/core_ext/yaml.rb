3279d4e2d282ead61461b383b28dee1aeafe4d2a
Scott Steadman <ss@stdmn.com>
2009-07-11 17:40:06 -0700

    Added yaml extension.

---
+++ lib/se/core_ext/yaml.rb
@@ -0,0 +1,13 @@
+
+
+#
+# Read a file, run it through ERB, then YAML::load.
+#
+# The caller is expected to have loaded ERB.
+#
+#  +filename+ - name of file
+#  +args+     - arguments for ERB.new call
+#
+def YAML.load_erb_file(filename, *args)
+  YAML::load(ERB.new(IO.read(filename), *args).result)
+end
